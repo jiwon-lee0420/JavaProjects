0994b9bd8445083db66719903098fdecfadfb7f8
jiwon-lee0420 <100016488+jiwon-lee0420@users.noreply.github.com>
2022-12-05 21:58:22 -0500

    HW09 v3
    figure out how to change total hours to completion.

--- ToDoList.java
+++ ToDoList.java
@@ -3,9 +3,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
-import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.TextAlignment;
+import javafx.scene.text.Font;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.BorderPane;
@@ -16,6 +15,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Control;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyEvent;
@@ -24,6 +24,8 @@ import javafx.geometry.Pos;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
 
 public class ToDoList extends Application {
     private final Alert alert1 = new Alert(AlertType.ERROR, "Please enter a task.");
@@ -31,75 +33,113 @@ public class ToDoList extends Application {
     private final Alert alert3 = new Alert(AlertType.ERROR, "Please select number of hours.");
     private final Alert alert4 = new Alert(AlertType.ERROR, "Exact task already exists.");
     private final Alert alert5 = new Alert(AlertType.ERROR, "Task name already exists.");
-    @Override
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
     public void start(Stage primaryStage) {
-        int numTasksCompleted = 0;
-        int numTasksRemaining = 0;
-        //ObservableList for ListView object...
-        ObservableList<String> tasks = FXCollections.observableArrayList();
-        //Title
+        //Text initialization
         Text title = new Text("To-Do List");
-        title.setFont(Font.font("Times New Roman", FontWeight.BOLD, 30));
-        title.setTextAlignment(TextAlignment.CENTER);
-        // numtaskscompleted and numtasksremaining text
-        Text ntc = new Text("Number of Tasks Completed:    " + numTasksCompleted);
-        Text ntr = new Text("Number of Tasks Completed:    " + numTasksRemaining);
-        //TextField
+        title.setFont(new Font("Times New Roman", 20));
+        //Label initialization
+        Label ntr = new Label("Number of tasks remaining:    0");
+        Label ntc = new Label("Number of tasks completed:    0");
+        Label totalHours = new Label("Total hours to complete task:    0");
+        //TextField initializations
         TextField tf = new TextField("Add task here...");
-        //ComboBoxes
+        tf.requestFocus();
+        //ComboBox initializations
         ComboBox<String> cbo1 = new ComboBox<String>();
         cbo1.getItems().addAll("Study", "Shop", "Cook", "Sleep");
         ComboBox<String> cbo2 = new ComboBox<String>();
         cbo2.getItems().addAll("1", "2", "3", "4", "5");
-        //Buttons
+        //Button initializations
         Button enq = new Button("Enqueue Task");
         Button deq = new Button("Dequeue Task");
-        //Hbox TOP
-        HBox hbTop = new HBox(ntc, ntr);
-        hbTop.setAlignment(Pos.CENTER);
-        hbTop.setSpacing(30);
-        //Hbox BOTTOM
-        HBox hbBottom = new HBox(tf, cbo1, cbo2, enq, deq);
-        hbBottom.setAlignment(Pos.CENTER);
-        hbBottom.setSpacing(20);
-        //Vbox
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(title, hbTop);
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setSpacing(10);
-        //ListView declaration;
-        ListView<String> lv = new ListView<String>(tasks);
-        //Enqueue button setOnAction
+        ObservableList<String> ol = FXCollections.observableArrayList();
+        //enqueue button action
         enq.setOnAction(e -> {
             if (tf.getText().equals("") 
                 || tf.getText().equals("Add task here...")) {
                 alert1.showAndWait();
+                return;
             } else if (cbo1.getValue() == null) {
                 alert2.showAndWait();
+                return;
             } else if (cbo2.getValue() == null) {
                 alert3.showAndWait();
+                return;
             }
-            for (int i = 0; i < tasks.size(); i++) {
-                if (tf.getText().equals(tasks.get(i).split(" ")[0])
-                    && cbo1.getValue().equals(tasks.get(i).split(" ")[7])
-                    && cbo2.getValue().equals(tasks.get(i).split(" ")[])) {
-
+            for (int i = 0; i < ol.size(); i++) {
+                if (tf.getText().equals(ol.get(i).split(" - ")[0])
+                    && cbo1.getValue().equals(ol.get(i).split(" - ")[1].split(" ")[3])
+                    && cbo2.getValue().equals(ol.get(i).split(" - ")[2].split(" ")[3])) {
+                    alert4.showAndWait();
+                    return;
+                } else if (tf.getText().equals(ol.get(i).split(" - ")[0])
+                           && cbo2.getValue().equals(ol.get(i).split(" - ")[2].split(" ")[3])) {
+                    alert5.showAndWait();
+                    return;
+                } else if (tf.getText().equals(ol.get(i).split(" - ")[0])
+                           && cbo1.getValue().equals(ol.get(i).split(" - ")[1].split(" ")[3])) {
+                    ol.set(i, tf.getText() + " - Type of task: " + cbo1.getValue() + " Complete by " + addHours(new java.util.Date(), Integer.parseInt(cbo2.getValue())));
+                    return;
                 }
             }
+            ol.add(tf.getText() + " - Type of task: " + cbo1.getValue() + " - Complete by " + addHours(new java.util.Date(), Integer.parseInt(cbo2.getValue())));
+            tf.setText("Add task here...");
+            tf.selectAll();
+            int th = Integer.parseInt(totalHours.getText().split(":")[1].split(" ")[4]) + Integer.parseInt(cbo2.getValue());
+            cbo1.setValue(null);
+            cbo2.setValue(null);
+            int numTasksRemaining = ol.size();
+            ntr.setText("Number of tasks remaining:    " + numTasksRemaining);
+            totalHours.setText("Total hours to complete task:    " + th);
         });
+        //dequeue button action
+        deq.setOnAction(e -> {
+            if (ol.size() == 0) {
+                return;
+            }
+            String removed = ol.remove(0);
+            int numTasksRemaining = ol.size();
+            ntr.setText("Number of tasks remaining:    " + numTasksRemaining);
+            int numTasksCompleted = Integer.parseInt(ntc.getText().split(":")[1].split(" ")[4]) + 1;
+            ntc.setText("Number of tasks completed:    " + numTasksCompleted);
+            //int th = Integer.parseInt(removed.split(" - "))
+        });
+        //ListView initialization
+        ListView<String> lv = new ListView<String>(ol);
+        lv.setPrefWidth(500);
+        lv.setMaxWidth(Control.USE_PREF_SIZE);
+        lv.setMinWidth(Control.USE_PREF_SIZE);
+        lv.setPrefHeight(250);
+        lv.setMaxHeight(Control.USE_PREF_SIZE);
+        lv.setMinHeight(Control.USE_PREF_SIZE);
+        //HBox TOP initialization
+        HBox hbTop = new HBox(ntr, ntc);
+        hbTop.setAlignment(Pos.CENTER);
+        hbTop.setSpacing(20);
+        //HBox BOTTOM initialization
+        HBox hbBottom = new HBox(tf, cbo1, cbo2, enq, deq);
+        hbBottom.setAlignment(Pos.CENTER);
+        hbBottom.setSpacing(20);
+        //VBox initializatoin
+        VBox vb = new VBox(title, hbTop, totalHours);
+        vb.setAlignment(Pos.CENTER);
+        vb.setSpacing(10);
         BorderPane root = new BorderPane();
         root.setCenter(lv);
-        root.setTop(vbox);
+        root.setTop(vb);
         root.setBottom(hbBottom);
         primaryStage.setTitle("To Do List");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
-    private String getDateTime(String hours) {
-        int h = Integer.parseInt(hours);
-        
-    }
-    public static void main(String[] args) {
-        launch(args);
+    private Date addHours(Date date, int hours) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.HOUR_OF_DAY, hours);
+        return calendar.getTime();
     }
 }
